91392f78efa548a770c7b932a311d43c005906cc
Yaroslav49 <122696419+Yaroslav49@users.noreply.github.com>
2025-07-01 15:29:01 +0300

    Добавлен новый раздел на welcome-страницу

---
+++ src/app/pages/welcome-page/info-card/info-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
+
+@Component({
+  selector: 'info-card',
+  imports: [TuiIcon],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.css'
+})
+export class InfoCardComponent {
+   @Input() iconName!: string;
+   @Input() text!: string;
+
+   getIconTag(): string {
+      return "@tui." + (this.iconName || 'ban');
+   }
+}
--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TuiButton } from '@taiga-ui/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { InfoCardComponent } from './info-card/info-card.component';
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [TuiButton, HeaderComponent, FooterComponent],
+  imports: [TuiButton, HeaderComponent, FooterComponent, InfoCardComponent],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
