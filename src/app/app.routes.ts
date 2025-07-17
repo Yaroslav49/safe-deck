a0cd39a12f652d365a322f1c0fa7508ebb7c0b6c
Yaroslav49 <122696419+Yaroslav49@users.noreply.github.com>
2025-07-17 21:59:00 +0300

    Сделан журнал просмотров паролей

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,12 @@ import { RolesPageComponent } from './pages/roles-page/roles-page.component';
 import { MembersPageComponent } from './pages/members-page/members-page.component';
 import { MemberPageComponent } from './pages/member-page/member-page.component';
 import { PasswordGeneratorComponent } from './pages/password-generator/password-generator.component';
+import { LogsComponent } from './pages/logs/logs.component';
 
 export const routes: Routes = [
    {path: 'boards', component: BoardsPageComponent, canActivate: [RouteGuardAuth]},
    {path: 'roles/:board-id', component: RolesPageComponent, canActivate: [RouteGuardAuth]},
+   {path: 'logs/:board-id', component: LogsComponent, canActivate: [RouteGuardAuth]},
    {path: 'members/:board-id', component: MembersPageComponent, canActivate: [RouteGuardAuth]},
    {path: 'member/:board-id/:member-id', component: MemberPageComponent, canActivate: [RouteGuardAuth]},
    {path: 'cards/:board-id', component: CardsPageComponent, canActivate: [RouteGuardAuth]},
